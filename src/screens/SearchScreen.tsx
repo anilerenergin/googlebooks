fc1f0619d3810bd5be38c10c8b358e59d76a9d06
Anıl Eren Ergin <anilerenerg@gmail.com>
2024-01-20 14:45:09 +0300

    first screen

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+
+const BookRequest: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Discover Books</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default BookRequest;
