9540e6bd2e583daf0154d1f8077e5ddd408363b9
Anıl Eren Ergin <anilerenerg@gmail.com>
2024-01-21 00:03:59 +0300

    expand book model

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,13 +1,85 @@
-export interface Book {
-    id: string; 
-    volumeInfo: {
-      title: string;
-      authors?: string[];
-      publishedDate?: string;
-      imageLinks?: {
-        thumbnail?: string;
-      };
-    };
-  }
-  
-  
+interface IndustryIdentifier {
+  type: string;
+  identifier: string;
+}
+
+interface ReadingModes {
+  text: boolean;
+  image: boolean;
+}
+
+interface ImageLinks {
+  smallThumbnail: string;
+  thumbnail: string;
+}
+
+interface VolumeInfo {
+  title: string;
+  subtitle?: string;
+  authors?: string[];
+  publisher?: string;
+  publishedDate?: string;
+  description?: string;
+  industryIdentifiers?: IndustryIdentifier[];
+  readingModes?: ReadingModes;
+  pageCount?: number;
+  printType?: string;
+  categories?: string[];
+  maturityRating?: string;
+  allowAnonLogging?: boolean;
+  contentVersion?: string;
+  panelizationSummary?: {
+    containsEpubBubbles: boolean;
+    containsImageBubbles: boolean;
+  };
+  imageLinks?: ImageLinks;
+  language?: string;
+  previewLink?: string;
+  infoLink?: string;
+  canonicalVolumeLink?: string;
+}
+
+interface SaleInfo {
+  country: string;
+  saleability: string;
+  isEbook: boolean;
+}
+
+interface AccessInfo {
+  country: string;
+  viewability: string;
+  embeddable: boolean;
+  publicDomain: boolean;
+  textToSpeechPermission: string;
+  epub: {
+    isAvailable: boolean;
+    acsTokenLink?: string;
+  };
+  pdf: {
+    isAvailable: boolean;
+    acsTokenLink?: string;
+  };
+  webReaderLink: string;
+  accessViewStatus: string;
+  quoteSharingAllowed: boolean;
+}
+
+interface SearchInfo {
+  textSnippet: string;
+}
+
+interface Book {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: VolumeInfo;
+  saleInfo: SaleInfo;
+  accessInfo: AccessInfo;
+  searchInfo: SearchInfo;
+}
+
+interface SearchBookType {
+  totalItems: number;
+  items: Book[];
+}
