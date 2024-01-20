39ff191141be1977fcd32ac8c0b6e4e89283d9d2
Anıl Eren Ergin <anilerenerg@gmail.com>
2024-01-20 14:51:58 +0300

    styling

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,15 +1,30 @@
 
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import { ScreenWidth, ScreenHeight } from 'react-native-elements/dist/helpers';
 
 
 const BookRequest: React.FC = () => {
   return (
-    <SafeAreaView>
-      <Text>Discover Books</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Discover Books</Text>
     </SafeAreaView>
   );
 };
 
-
+const styles =  StyleSheet.create({
+    container: {
+        width: ScreenWidth,
+        height: ScreenHeight,
+        backgroundColor: '#f0f0f0',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+      },
+})
 export default BookRequest;
