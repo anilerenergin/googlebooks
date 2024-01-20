fb8c3c0e3bfc2f7d4101ee0f84fd738df56ddbca
Anıl Eren Ergin <anilerenerg@gmail.com>
2024-01-20 15:21:34 +0300

    printing book names on screen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,23 +1,37 @@
 
-import React, { useEffect } from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Text, SafeAreaView, StyleSheet, TouchableOpacity, View, FlatList} from 'react-native';
 import { ScreenWidth, ScreenHeight } from 'react-native-elements/dist/helpers';
 import searchBooks from '../api/searchBooks';
+import { Book } from '../types/book';
 
-
-const BookRequest: React.FC = () => {
+interface BookRequestProps {
+    books: Book[];
+  }
+  
+const BookRequest: React.FC<BookRequestProps> = () => {
+    const [books, setBooks] = useState<Book[]>([]);
     useEffect(()=>{
-        searchBooks("tolkien",0,10).then((e)=>{
+     searchBooks("tolkien",0,10).then((e)=>{
+        setBooks(e)
             e.forEach((b)=>{console.log(b.volumeInfo.title)}),[]
         },);
-    })
+    },[])
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.header}>Discover Books</Text>
+      <FlatList  data={books}
+          keyExtractor={item => item.id}
+          renderItem={bookList}
+          numColumns={1}></FlatList>
     </SafeAreaView>
   );
 };
-
+const bookList  = ({item}: {item: any}) => (
+    <TouchableOpacity>
+          <Text>{item.volumeInfo.title}</Text>
+    </TouchableOpacity>
+  );
 const styles =  StyleSheet.create({
     container: {
         width: ScreenWidth,
